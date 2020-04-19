e7cc409f10766994670247fea223419bb1f08cf4
Agota <agota.kazenaite@yahoo.com>
2020-04-19 22:47:23 +0300

    added h2 DB

--- src/main/java/entities/Trip.java
+++ src/main/java/entities/Trip.java
@@ -1,10 +1,27 @@
 
 package entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
+import java.util.Objects;
 
+
+@Entity
+@Table(name = "TRIP")
 public class Trip implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Size(max = 50)
+    @Column(name = "NAME")
     private String name;
 
     public Trip() {
@@ -20,5 +37,28 @@ public class Trip implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Trip trip = (Trip) o;
+        return Objects.equals(id, trip.id) &&
+                Objects.equals(name, trip.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 }
